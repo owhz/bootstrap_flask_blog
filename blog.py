a0e802f871a6c5bf358f0bddcea1d9f74754a430
alanowen <alanowen3310@hotmail.com>
2018-11-04 20:08:29 +0800

    modify docker config

--- blog.py
+++ blog.py
@@ -1,20 +1,11 @@
 import os
 
 from flask_migrate import Migrate, upgrade, current_app
-from dotenv import load_dotenv
-
-
-flask_env = os.environ.get('FLASK_ENV')
-
-if not flask_env:
-    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-    if os.path.exists(dotenv_path):
-        load_dotenv(dotenv_path)
-
 
 from app import create_app
 from app.models import db, User, Post
 
+flask_env = os.environ.get('FLASK_ENV')
 
 app = create_app(os.getenv('FLASK_ENV') or 'default')
 Migrate(app, db)
--- config.py
+++ config.py
@@ -1,15 +1,17 @@
 import os
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    SECRET_KEY = 'hard to guess'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'hard to guess')
-    BLOG_ADMIN_EMAIL = os.environ.get('BLOG_ADMIN_EMAIL', 'admin@example.com')
-    BLOG_ADMIN_USERNAME = os.environ.get('BLOG_ADMIN_USERNAME', 'admin')
-    BLOG_ADMIN_PASSWORD = os.environ.get('BLOG_ADMIN_PASSWORD', 'admin')
+
+    BLOG_ADMIN_EMAIL = 'admin@admin.com'
+    BLOG_ADMIN_USERNAME = 'admin'
+    BLOG_ADMIN_NAME = 'admin'
+    BLOG_ADMIN_PASSWORD = 'admin'
 
     @classmethod
     def init_app(cls, app):
@@ -17,7 +19,7 @@ class Config:
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/blog'
 
     @classmethod
     def init_app(cls, app):
@@ -31,7 +33,7 @@ class ProductionConfig(Config):
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/blog'
 
 
 class TestingConfig(Config):
